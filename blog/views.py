c81b1a6400282fdc0844cc18cea9695f47d05139
justSevbo <sevbofx@gmail.com>
2022-02-16 23:12:03 +0500

    homepage updated with models

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Author, Tags, Post
 
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    top_posts = Post.objects.filter(top=True)[:3]
+    context = {
+        'posts': posts,
+        'top_posts': top_posts
+    }
+    return render(request, 'index.html', context)
--- config/urls.py
+++ config/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', include('blog.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, documet_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, documet_root=settings.STATICFILES_DIRS)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
