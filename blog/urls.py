c5eefe80f3cf578ece8e28a103ed79be1e7d64b5
justSevbo <sevbofx@gmail.com>
2022-02-17 00:17:23 +0500

    Category page created

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views as blog_views
 
 
 urlpatterns = [
-    path('', blog_views.index, name='home')
+    path('', blog_views.index, name='home'),
+    path('uz/', blog_views.uz_lang_posts, name='home_uz'),
+    path('category/<str:category>/<int:id>/', blog_views.cat_posts, name='category')
 ]
--- blog/views.py
+++ blog/views.py
@@ -23,3 +23,43 @@ def index(request):
         'top_posts': top_posts,
     }
     return render(request, 'index.html', context)
+
+
+def uz_lang_posts(request):
+    key = request.GET.get('search')
+    if key:
+        posts = Post.objects.filter(Q(title__icontains=key) | Q(category__category__icontains=key)).filter(language__exact='uz')
+    else:
+        posts = Post.objects.filter(language__exact='uz')
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(posts, 4)
+    try:
+        posts = paginator.page(page_num)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    top_posts = Post.objects.filter(top=True)[:3]
+    context = {
+        'posts': posts,
+        'top_posts': top_posts,
+    }
+    return render(request, 'uzb_lang_posts.html', context)
+
+
+def cat_posts(request, id, category):
+    cat = get_object_or_404(Category, id=id, category=category)
+    posts = Post.objects.filter(category=cat)
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(posts, 4)
+    try:
+        posts = paginator.page(page_num)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    context = {
+        'cat': cat,
+        'posts': posts
+    }
+    return render(request, 'category.html', context)
