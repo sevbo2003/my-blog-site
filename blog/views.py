2db0239e978d87a5fb3cd6af76bd86d99a433790
justSevbo <sevbofx@gmail.com>
2022-02-16 23:38:11 +0500

    Search method added

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Author, Tags, Post
-
+from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
-    posts = Post.objects.all()
+    key = request.GET.get('search')
+    if key:
+        posts = Post.objects.filter(Q(title__icontains=key) | Q(category__category__icontains=key))
+    else:
+        posts = Post.objects.all()
+    # page_num = request.GET.get('page', 1)
+    # paginator = Paginator(posts, 1)
+    # try:
+    #     posts = paginator.page(page_num)
+    # except PageNotAnInteger:
+    #     posts = paginator.page(1)
+    # except EmptyPage:
+    #     posts = paginator.page(paginator.num_pages)
     top_posts = Post.objects.filter(top=True)[:3]
     context = {
         'posts': posts,
