3639069d2472f45941877380d49182eb34a29622
justSevbo <sevbofx@gmail.com>
2022-02-16 23:46:50 +0500

    Paginator added

--- blog/views.py
+++ blog/views.py
@@ -9,17 +9,17 @@ def index(request):
         posts = Post.objects.filter(Q(title__icontains=key) | Q(category__category__icontains=key))
     else:
         posts = Post.objects.all()
-    # page_num = request.GET.get('page', 1)
-    # paginator = Paginator(posts, 1)
-    # try:
-    #     posts = paginator.page(page_num)
-    # except PageNotAnInteger:
-    #     posts = paginator.page(1)
-    # except EmptyPage:
-    #     posts = paginator.page(paginator.num_pages)
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(posts, 4)
+    try:
+        posts = paginator.page(page_num)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     top_posts = Post.objects.filter(top=True)[:3]
     context = {
         'posts': posts,
-        'top_posts': top_posts
+        'top_posts': top_posts,
     }
     return render(request, 'index.html', context)
